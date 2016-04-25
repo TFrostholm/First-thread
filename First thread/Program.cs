04a29a7e0d02091250d87f6bbc160eb982ff76e4
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-25 17:31:22 +0200

    Everything but the Pool assignment

--- First thread/Program.cs
+++ First thread/Program.cs
@@ -16,31 +16,46 @@ namespace First_thread
             //Print();
             //Print();
 
-            Thread t1 = new Thread(() => Print("First", 10));
-            Thread t2 = new Thread(() => Print("Second", 10));
-            Thread t3 = new Thread(() => Print("Third", 10));
-            Thread t4 = new Thread(() => Print("Fourth", 10));
+            Thread t1 = new Thread(() => Print("Hello", 10, 2));
+            Thread t2 = new Thread(() => Print("everybody!", 10, 10));
+            Thread t3 = new Thread(() => Print("What's", 10, 1000));
+            Thread t4 = new Thread(() => Print("up?", 10, 1500));
 
-            t1.Name = "My first thread";
-            t2.Name = "My second thread";
-            t3.Name = "My third thread";
-            t4.Name = "My fourth thread";
+            t1.Name = "1";
+            t2.Name = "2";
+            t3.Name = "3";
+            t4.Name = "4";
+
+            t1.Priority = ThreadPriority.Lowest;
+            t2.Priority = ThreadPriority.BelowNormal;
+            t3.Priority = ThreadPriority.Highest;
+            t4.Priority = ThreadPriority.Normal;
 
             t1.Start();
             t2.Start();
             t3.Start();
             t4.Start();
 
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+
+            Console.WriteLine("\nThis is it");
+
+            //Not working correctly - didn't fully understand whis Pooling thing
+            Task.Run(() => Print("Pool", 2, 1));
+            Console.ReadLine();
+
         }
 
-        static void Print(string message, int numberOfTimes)
+        static void Print(string message, int numberOfTimes, int sleepTime)
         {
             for (int i = 0; i < numberOfTimes; i++)
             {
-                Console.WriteLine("{0}, {1}", message, Thread.CurrentThread.Name);              
+                Console.WriteLine("Message: {0}, Thread number: {1} has it's priority set to {2}", message, Thread.CurrentThread.Name, Thread.CurrentThread.Priority);
+                Thread.Sleep(sleepTime);              
             }
         }
-
-        //TODO try out the Sleep method and the rest of Lab_First thread
     }
 }
